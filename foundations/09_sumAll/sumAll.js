eda03e1ca57e64da03a81abe2204e3a1364fd0fe
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-16 22:06:36 +0000

    completed exercise 10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (intA, intB) {
+    let sum = 0;
+    if (
+        ((intA || intB) < 0) ||
+        ((intA % 1 || intB % 1) !== 0) ||
+        (typeof intA !== 'number' || typeof intB !== 'number')
+    ) {
+        return 'ERROR'
+    } else if (intA < intB) {
+        while (intA <= intB) {
+            sum += intA;
+            intA++;
+        }
+    } else {
+        while (intB <= intA) {
+            sum += intB;
+            intB++;
+        }
+    }
 
+
+    return sum;
 };
 
 // Do not edit below this line

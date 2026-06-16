5341852a0a49b7eeee857ebbaf127900f1ad2e5e
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-16 19:42:15 +0000

    complete exercise 6

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, repeatNum) {
+    if (repeatNum < 0) {
+        return 'ERROR';
+    } else {
+        let repeatedString = '';
+        for (let i = repeatNum; i > 0; i--) {
+            repeatedString += str;
+        }
 
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line

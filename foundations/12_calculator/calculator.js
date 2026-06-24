86d542432f0d5c4ac57422805fe657c9672edfb7
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-24 18:26:48 +0000

    finished exercise 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  return arr.reduce((sum, num) => sum + num, 0)
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((prod, num) => prod * num, 1)
 };
 
-const power = function() {
-	
+const power = function (base, expo) {
+  return (base ** expo);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let numList = [];
+  for (let i = 1; i <= num; i++) {
+    numList.push(i);
+  }
+
+  return numList.reduce((prod, num) => prod * num, 1)
 };
 
 // Do not edit below this line

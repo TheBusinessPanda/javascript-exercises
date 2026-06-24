fda846fbe116f91ce0965ead53e9cf491a8853d5
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-24 18:56:19 +0000

    completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const validVals = 'abcdefghigklmnopqrstuvwxyz123456789'
+    let reversedStr = str.toLowerCase().split('').filter((char) => validVals.includes(char)).reverse().join('');
+    let newStr = str.toLowerCase().split('').filter((char) => validVals.includes(char)).join('');
 
+    return newStr === reversedStr
 };
 
 // Do not edit below this line

c51a8f66ced9bbce97e4c07bf44c7b0f59a1cd55
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-25 22:05:45 +0000

    completed exercise 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    let titles = [];
+    for (let auths of books) {
+        titles.push(auths.title);
+    }
 
+    return titles;
 };
 
 // Do not edit below this line

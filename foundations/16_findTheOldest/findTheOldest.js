de3da4c47ffeccc80b24f9515183ed734ddd46d0
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-25 22:28:42 +0000

    completed exercise 16 and foundations

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (peoples) {
+    let ages = [];
+    for (let people of peoples) {
+        ages.push((people.yearOfDeath || 2026) - people.yearOfBirth)
+    }
 
+    const highestAgeIndex = ages.indexOf(Math.max(...ages));
+    return peoples[highestAgeIndex];
 };
 
 // Do not edit below this line

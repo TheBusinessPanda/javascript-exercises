c5acc9d0d386c5bcfea353c0362001001c4dc6ae
TheBusinessPanda <mastermrinank@gmail.com>
2026-06-25 21:56:18 +0000

    Completed fibonacci exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+let fibonacci = function (place) {
+    let fibonacciSqn = [0, 1];
+    let i = 1;
 
+    if (place >= 0) {
+        while (fibonacciSqn.length <= place) {
+            fibonacciSqn.push(i);
+            i = fibonacciSqn[fibonacciSqn.length - 2] + fibonacciSqn[fibonacciSqn.length - 1]
+        }
+    } else {
+        return 'OOPS'
+    }
+
+    return fibonacciSqn[place];
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+//fibonacciSqn = [1, 1, 2, 3, ];
